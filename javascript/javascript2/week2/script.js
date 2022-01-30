6f0186966fcd4c7187df7d8c2a8d12f2df1c50dd
Ervins-Trans <ervins.trans@gmail.com>
2022-01-30 18:58:00 +0100

    Warmup done

--- javascript/javascript2/week2/script.js
+++ javascript/javascript2/week2/script.js
@@ -1,3 +1,12 @@
+// Warmup
+
+const numbers = [1, 2, 3, 4]; // arr
+const newNumbers = numbers // newNumbers are declared,
+  .filter((e) => e % 2 !== 0) // and are filtered to odd numbers
+  .map((newElement) => newElement * 2); // are multiplied by 2 and returned to the newNumbers array
+console.log(`The doubled numbers are ${newNumbers}`); // 2, 6 (1*2, 3*2)
+
+// Movies
 const wrapper = document.querySelector(".wrapper");
 
 wrapper.innerText = " Javascript Override ";
