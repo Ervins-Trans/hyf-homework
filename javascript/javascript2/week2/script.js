6a5635fc52c0cdae915df95cf464e40e092eb663
Ervins-Trans <ervins.trans@gmail.com>
2022-01-30 20:59:48 +0100

    simple calculations over movies arr done

--- javascript/javascript2/week2/script.js
+++ javascript/javascript2/week2/script.js
@@ -7,8 +7,23 @@ const newNumbers = numbers // newNumbers are declared,
 console.log(`The doubled numbers are ${newNumbers}`); // 2, 6 (1*2, 3*2)
 
 // Movies
-const wrapper = document.querySelector(".wrapper");
 
-wrapper.innerText = " Javascript Override ";
+const shortTitledMovies = movies //new array for short movies
+  .filter((movie) => movie.title.length <= 3) // movies are filtered out if their title is 4 or longer
+  .map((filteredMovie) => filteredMovie); // matching criteria movies are mapped to new array.
+console.log(shortTitledMovies);
 
-const importedMovies = movies;
+const longTitledMovies = movies //new array for long movies
+  .filter((movie) => movie.title.length >= 20) // movies are filtered out if their title is 19 or shorter. Good case for an input or default lenght of some text.
+  .map((filteredMovie) => filteredMovie); // matching criteria movies are mapped to new array.
+console.log(longTitledMovies); // more than 1400 titles........ huh.
+
+let amountOfMoviesInTenYears = 0; // long name for a short variable
+movies.forEach((movie) =>
+  movie.year >= 1980 && movie.year <= 1989
+    ? amountOfMoviesInTenYears++
+    : amountOfMoviesInTenYears
+); // shorthand goes brrrrrrrrrrr ( for each movie check if filming year falls between 1980 and 1989, if true - increase, otherwise not)
+console.log(
+  `${amountOfMoviesInTenYears} movies were filmed in between 1980 and 1989` // more than 600 movies in 10 years, truly golden age of cinematography
+);
